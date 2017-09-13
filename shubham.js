ce2cad1731958c8ebb0c44536498773a519edfc6
Shubham Kamboj <IIITU16118@GMAIL.COM>
2017-09-13 06:03:17 -0700

    Update shubham.js

--- shubham.js
+++ shubham.js
@@ -1,20 +1,15 @@
-$(document).ready(function() {
-  $("#home-content").css('padding-top', $(window).height() * .2);
-  $("#about-content").css('padding-top', $(window).height() * .23);
-  $("#portfolio-content").css('padding-top', $(window).height() * .23);
-  $("#contact-content").css('padding-top', $(window).height() * .23);
-  $("#home").css('min-height', $(window).height() * .8);
-  $("#about").css('min-height', $(window).height() * .8);
-  $("#portfolio").css('min-height', $(window).height() * .8);
-  $("#contact").css('min-height', $(window).height() * .95);
-
-  $("#footer").css('height', $(window).height() * .08);
-
-  $(window).resize(function() {
-
-  })
-})
-
-function newWindow(x) {
-  window.open(x);
-}
+// Smooth Scrolling
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
